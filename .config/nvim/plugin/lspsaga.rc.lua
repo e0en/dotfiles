76e1ddb3783769df5abee6d51723680f806a5151
Yoonseop Kang <e0engoon@gmail.com>
2022-10-05 16:22:05 +0900

    👽 Update neovim lspsaga config

--- .config/nvim/plugin/lspsaga.rc.lua
+++ .config/nvim/plugin/lspsaga.rc.lua
@@ -5,20 +5,10 @@ if (not status) then return end
 saga.init_lsp_saga()
 
 local opt = { silent = true, noremap = true }
-local action = require('lspsaga.action')
--- scroll up/down hover doc or scroll in definition preview
-vim.keymap.set('n', '<C-f>', function()
-    action.smart_scroll_with_saga(1)
-end, opt)
-vim.keymap.set('n', '<C-b>', function()
-    action.smart_scroll_with_saga(-1)
-end, opt)
 
 -- preview things
 vim.keymap.set('n', 'gp', '<cmd>Lspsaga preview_definition<CR>', opt)
 vim.keymap.set('n', 'K', '<cmd>Lspsaga hover_doc<CR>', opt)
 
 -- code manipulation
-local codeaction = require('lspsaga.codeaction')
-vim.keymap.set('n', '<leader>ca', codeaction.code_action, opt)
-vim.keymap.set("v", "<leader>ca", "<cmd><C-U>Lspsaga range_code_action<CR>", { silent = true })
+vim.keymap.set({"n","v"}, "<leader>ca", "<cmd>Lspsaga code_action<CR>", { silent = true })

472a8e3e7609e0e24621e2e5db113d364f58c657
Yoonseop Kang <e0engoon@gmail.com>
2023-05-16 13:41:14 +0900

    ⚡️ Use ruff to format & lint python files

--- .config/nvim/plugin/null-ls.rc.lua
+++ .config/nvim/plugin/null-ls.rc.lua
@@ -7,18 +7,12 @@ local augroup = vim.api.nvim_create_augroup('LspFormatting', {})
 null_ls.setup({
   sources = {
     null_ls.builtins.formatting.black,
-    null_ls.builtins.formatting.isort.with({
-      extra_args = { '--profile', 'black' },
-    }),
+    null_ls.builtins.formatting.ruff,
     null_ls.builtins.formatting.prettierd,
     null_ls.builtins.formatting.shellharden,
     null_ls.builtins.formatting.shfmt,
-    null_ls.builtins.diagnostics.pylint.with({
-      diagnostics_postprocess = function(diagnostic)
-        diagnostic.code = diagnostic.message_id
-      end,
-    }),
     null_ls.builtins.formatting.clang_format,
+    null_ls.builtins.diagnostics.ruff,
   },
   on_attach = function(client, bufnr)
     if client.supports_method('textDocument/formatting') then

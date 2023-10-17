af42ef959aff5dc3f60024e39c432d47e5c3dc49
Yoonseop Kang <e0engoon@gmail.com>
2023-10-17 17:15:00 +0900

    🔧 Replace rio with wezterm

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -1,30 +1,30 @@
 local wezterm = require("wezterm")
 
 return {
-  -- window appearance
-  use_fancy_tab_bar = true,
-  hide_tab_bar_if_only_one_tab = true,
-  window_decorations = "INTEGRATED_BUTTONS|RESIZE",
-  window_padding = {
-    left = "1cell",
-    right = "1cell",
-    top = "2cell",
-    bottom = "0.5cell",
-  },
-  initial_cols = 80,
-  initial_rows = 48,
+	-- window appearance
+	use_fancy_tab_bar = true,
+	hide_tab_bar_if_only_one_tab = false,
+	window_decorations = "INTEGRATED_BUTTONS|RESIZE",
+	window_padding = {
+		left = "0cell",
+		right = "0cell",
+		top = "0cell",
+		bottom = "0cell",
+	},
+	initial_cols = 80,
+	initial_rows = 48,
 
-  -- cursor
-  default_cursor_style = "BlinkingBlock",
-  cursor_blink_rate = 200,
-  animation_fps = 60,
+	-- cursor
+	default_cursor_style = "BlinkingBlock",
+	cursor_blink_rate = 200,
+	animation_fps = 60,
 
-  -- font
-  font = wezterm.font("Berkeley Mono", { weight = "Regular", stretch = "Normal", style = "Normal" }),
-  font_size = 12.0,
-  unicode_version = 14,
+	-- font
+	font = wezterm.font("Berkeley Mono", { weight = "Regular", stretch = "Normal", style = "Normal" }),
+	font_size = 12.0,
+	unicode_version = 14,
 
-  -- color
-  -- https://wezfurlong.org/wezterm/colorschemes/index.html
-  color_scheme = "Deafened (terminal.sexy)",
+	-- color
+	-- https://wezfurlong.org/wezterm/colorschemes/index.html
+	color_scheme = "ayu",
 }

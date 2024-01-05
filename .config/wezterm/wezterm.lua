3a74dadb11a45779121bffe7e00395eee843dfcc
Yoonseop Kang <e0engoon@gmail.com>
2024-01-05 17:19:25 +0900

    Add polybar, rice more

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -1,30 +1,40 @@
 local wezterm = require("wezterm")
 
-return {
-	-- window appearance
-	use_fancy_tab_bar = true,
-	hide_tab_bar_if_only_one_tab = false,
-	window_decorations = "INTEGRATED_BUTTONS|RESIZE",
-	window_padding = {
-		left = "0cell",
-		right = "0cell",
-		top = "0cell",
-		bottom = "0cell",
-	},
-	initial_cols = 80,
-	initial_rows = 48,
+if wezterm.target_triple == "aarch64-apple-darwin" then
+  config = {
+    -- window appearance
+    use_fancy_tab_bar = true,
+    hide_tab_bar_if_only_one_tab = false,
+    window_decorations = "INTEGRATED_BUTTONS|RESIZE",
+    window_padding = {
+      left = "0cell",
+      right = "0cell",
+      top = "0cell",
+      bottom = "0cell",
+    },
+    initial_cols = 80,
+    initial_rows = 48,
 
-	-- cursor
-	default_cursor_style = "BlinkingBlock",
-	cursor_blink_rate = 200,
-	animation_fps = 60,
+    -- cursor
+    default_cursor_style = "BlinkingBlock",
+    cursor_blink_rate = 200,
+    animation_fps = 60,
 
-	-- font
-	font = wezterm.font("Berkeley Mono", { weight = "Regular", stretch = "Normal", style = "Normal" }),
-	font_size = 12.0,
-	unicode_version = 14,
+    font_size = 12.0,
+  }
+elseif wezterm.target_triple == "x86_64-unknown-linux-gnu" then
+  config = {
+    use_fancy_tab_bar = false,
+    hide_tab_bar_if_only_one_tab = true,
+    font_size = 10.0,
+  }
+else
+  config = {}
+end
 
-	-- color
-	-- https://wezfurlong.org/wezterm/colorschemes/index.html
-	color_scheme = "ayu",
-}
+-- https://wezfurlong.org/wezterm/colorschemes/index.html
+config.color_scheme = "ayu"
+config.font = wezterm.font("Berkeley Mono", { weight = "Regular", stretch = "Normal", style = "Normal" })
+config.unicode_version = 1
+
+return config
